c5dd2985a64e961f691fbe579bffaa80bc3089bf
Collins Lemashon Sarisar <133764870+Lemaashon@users.noreply.github.com>
2025-10-07 07:43:19 +0100

    Add project files.

---
+++ Lema/Application.cs
@@ -0,0 +1,26 @@
+﻿using Lema.Commands;
+using Nice3point.Revit.Toolkit.External;
+
+namespace Lema
+{
+    /// <summary>
+    ///     Application entry point
+    /// </summary>
+    [UsedImplicitly]
+    public class Application : ExternalApplication
+    {
+        public override void OnStartup()
+        {
+            CreateRibbon();
+        }
+
+        private void CreateRibbon()
+        {
+            var panel = Application.CreatePanel("Commands", "Lema");
+
+            panel.AddPushButton<StartupCommand>("Execute")
+                .SetImage("/Lema;component/Resources/Icons/RibbonIcon16.png")
+                .SetLargeImage("/Lema;component/Resources/Icons/RibbonIcon32.png");
+        }
+    }
+}
