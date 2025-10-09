3e288fb5dc09471ba00d26333e1896a0c38598b7
Collins Lemashon Sarisar <133764870+Lemaashon@users.noreply.github.com>
2025-10-09 07:34:53 +0100

    Added Forms

---
+++ Lema/Forms/Custom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSSE.Forms
+{
+    public static class Custom
+    {
+    }
+    public class FormResult
+    {
+        private bool cancelled;
+        public bool ExampleCancelled 
+        {
+            get { return cancelled; }
+            set { cancelled = value; } 
+        }
+        public object Object { get; set; }
+        public List<object> Objects { get; set; } 
+        public bool Cancelled { get; set; }
+        public bool Valid { get; set; }
+        public bool Affirmatice { get; set; }
+    }
+}
