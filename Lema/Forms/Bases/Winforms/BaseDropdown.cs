2f902765105935b09795e606b9c254e62d6531b6
Collins Lemashon Sarisar <133764870+Lemaashon@users.noreply.github.com>
2025-10-27 08:15:40 +0000

    WIndows Forms

---
+++ Lema/Forms/Bases/Winforms/BaseDropdown.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Form = System.Windows.Forms.Form;
+
+namespace Lema.Forms.Base
+{
+    public partial class BaseDropdown : Form
+    {
+        public BaseDropdown()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseDropdown_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lema/_Utilities/File_Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Form = System.Windows.Forms.Form;
+using System.Drawing;
+
+namespace Lema._Utilities
+{
+    public static class File_Utils
+    {
+        /// <summary>
+        /// Sets the icon of the specified form using the provided icon path or a default icon.
+        /// </summary>
+        /// <remarks>If the <paramref name="iconPath"/> is null, the method uses a default icon embedded
+        /// in the assembly. The method attempts to retrieve the icon from the specified path as an embedded
+        /// resource.</remarks>
+        /// <param name="form">The form whose icon is to be set. Cannot be null.</param>
+        /// <param name="iconPath">The path to the icon resource. If null, a default icon is used.</param>
+        public static void SetFormIcon(Form form, string iconPath = null)
+        {
+            iconPath ??= "Lema.Resources.Icons16.IconList16.ico";
+
+            using (var stream = Global.Assembly.GetManifestResourceStream(iconPath))
+            {
+                if (stream is not null)
+                {
+                    form.Icon = new Icon(stream);
+                }
+            }
+        }
+    }
+}
